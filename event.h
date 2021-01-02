0ce6316bbd43a52cabcae4aa852393534da97555
rws0116 <rws0116@unt.edu>
2021-01-02 11:17:18 -0500

    main and some psudocode for event class

---
+++ event.h
@@ -0,0 +1,28 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include<string>
+#include<vector>
+
+using namespace std;
+
+//waht do i want the event class to do?
+    //FightDragon(entity player)
+    //FightBear
+    //
+
+
+class event{
+
+    public:
+
+
+    private:
+    string initialMessage;
+    //tree of decisions? vector?
+
+
+};
+
+
+#endif
---
+++ spot.h
@@ -0,0 +1,21 @@
+#ifndef SPOT_H
+#define SPOT_H
+
+class spot{
+
+    public:
+    
+
+
+
+    private:
+
+
+
+
+};
+
+
+
+
+#endif
